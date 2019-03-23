d287226e25887aefcd0815f8bb838f7edce62b72
yogitachaudhari <yogitachaudhari1993@gmail.com>
2019-03-23 14:39:22 +0530

    third commit

--- GitEclipseDemo/src/GitdemoMyrepo.java
+++ GitEclipseDemo/src/GitdemoMyrepo.java
@@ -3,7 +3,7 @@ public class GitdemoMyrepo {
 
 	public static void main(String[] args) {
  System.out.println("hello");
- System.out.println("My firstasf commit message");
+ System.out.println("My first yogi commit message");
 	}
 
 }
