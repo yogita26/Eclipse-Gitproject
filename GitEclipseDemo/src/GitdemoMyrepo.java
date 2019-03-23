bed54ace8d457c6e85530b471d8d3d1b7439e86a
yogitachaudhari <yogitachaudhari1993@gmail.com>
2019-03-23 14:56:17 +0530

    old branch change

--- GitEclipseDemo/src/GitdemoMyrepo.java
+++ GitEclipseDemo/src/GitdemoMyrepo.java
@@ -4,6 +4,7 @@ public class GitdemoMyrepo {
 	public static void main(String[] args) {
  System.out.println("hello");
  System.out.println("My first yogi commit message");
+ System.out.println("changes by old branch");
 	}
 
 }
